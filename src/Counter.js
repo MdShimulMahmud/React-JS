4ab5816831af59baf049cbbc1204af4dc4749f78
Shimul Mahmud <shimulmahmud18@gmail.com>
2021-08-09 11:20:59 +0600

    initial commit

---
+++ src/Counter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function increment(prevState) {
+    return {
+        count: prevState.count + 1,
+    };
+}
+
+export default class Counter extends React.Component {
+    state = { count: 0 };
+
+    handleClick = () => {
+        this.setState(increment);
+    };
+
+    render() {
+        const { count } = this.state;
+        return (
+            <div className="counter">
+                <h3>{count}</h3>
+                <button type="button" onClick={this.handleClick}>
+                    Click here
+                </button>
+            </div>
+        );
+    }
+}
