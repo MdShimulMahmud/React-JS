698a777cc106cd7e140b612786cb19da5fa5f1aa
Shimul Mahmud <shimulmahmud18@gmail.com>
2021-08-13 23:30:08 +0600

    changes

--- src/components/Hover.js
+++ src/components/Hover.js
@@ -1,26 +1,5 @@
-import React from 'react';
-
-<<<<<<< HEAD
-function Hover({ count, handleClick, theme, changeTheme }) {
-    const style =
-        theme === 'dark'
-            ? {
-                  backgroundColor: 'red',
-              }
-            : { backgroundColor: 'green' };
-
-=======
-function Hover({ count, handleClick }) {
->>>>>>> d1bc18a73ac60ef3e3d1053d1aa0295a7a80f641
-    return (
-        <div className="counter">
-            <h3 onMouseOver={handleClick} onFocus style={style}>
-                Hover {count} here
-            </h3>
-            <button type="button" onClick={changeTheme}>
-                change theme
-            </button>
-        </div>
-    );
+function Hover() {
+    return 'Shimul';
 }
+
 export default Hover;
