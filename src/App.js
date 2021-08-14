15a0d2b21326b7eff60784b16169de391baf70d5
Shimul Mahmud <shimulmahmud18@gmail.com>
2021-08-14 10:11:20 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Counter from './components/Counter';
+import Hover from './components/Hover';
 
 function App() {
     return (
         <div>
             <Counter />
+            <Hover />
         </div>
     );
 }
--- src/components/Hover.js
+++ src/components/Hover.js
@@ -1,5 +1,15 @@
+import { useState } from 'react';
+
 function Hover() {
-    return 'Shimul';
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <h2 onMouseOver={() => setCount((prevState) => prevState + 1)} onFocus>
+                {' '}
+                hover {count} here
+            </h2>
+        </div>
+    );
 }
 
 export default Hover;
