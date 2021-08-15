54fd96173d95a642e37913f490f4fcfc137118a4
Shimul Mahmud <shimulmahmud18@gmail.com>
2021-08-15 21:14:51 +0600

    init

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import ForwardInput from './components/ShowInput';
 
 function App() {
     const inputRef = useRef('');
+
     useEffect(() => {
         inputRef.current.focus();
     }, []);
+
     return (
         <div>
             <ForwardInput ref={inputRef} type="text" placeholder="write here" />
